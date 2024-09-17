4859c4eddfcabf3ae22186082290973dbab15e76
Droopytex <a.velasquezvega@gmail.com>
2024-09-17 11:23:33 -0300

    ajustes a manejo de mensajes try / catch

--- backend/funciones.js
+++ backend/funciones.js
@@ -8,54 +8,33 @@ const pool = new Pool({
 });
 
 const leerPost = async () => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM posts;");
-    return rows;
-  } catch (error) {
-    throw { code: 500, message: "Error al leer los posts: " + error.message };
-  }
+  const { rows } = await pool.query("SELECT * FROM posts;");
+  return rows;
 };
 
-const escribirPost = async (titulo, url, descripcion) => {
-  const consulta = "INSERT INTO posts values (DEFAULT, $1,$2,$3,0)";
-  const values = [titulo, url, descripcion];
-  try {
-    await pool.query(consulta, values);
-    console.log("Post agregado ");
-  } catch (error) {
-    throw { code: 500, message: "Error al escribir el post: " + error.message };
-  }
+const escribirPost = async (titulo, img, descripcion) => {
+  const consulta = "INSERT INTO posts values (DEFAULT, $1,$2,$3,0) RETURNING *";
+  const values = [titulo, img, descripcion];
+  const { rows } = await pool.query(consulta, values);
+  console.log("Post agregado ");
+  return 0;
 };
 
 const agregarLike = async (id) => {
   const consulta = "UPDATE posts SET likes = (likes + 1) WHERE id = $1;";
   const values = [id];
-  try {
-    const { rowCount } = await pool.query(consulta, values);
-    if (rowCount == 0) {
-      throw { code: 404, message: "No se consiguió ningún posts con este id" };
-    }
-  } catch (error) {
-    throw {
-      code: error.code || 500,
-      message: "Error al agregar like: " + error.message,
-    };
+  const { rowCount } = await pool.query(consulta, values);
+  if (rowCount === 0) {
+    throw { code: 404, message: "el posteo no existe con el ID =" + id };
   }
 };
 
 const borrarPost = async (id) => {
   const consulta = "DELETE FROM posts WHERE id = $1";
   const values = [id];
-  try {
-    await pool.query(consulta, values);
-    if (rowCount == 0) {
-      throw { code: 404, message: "No se encontró ningún post con este id" };
-    }
-  } catch (error) {
-    throw {
-      code: error.code || 500,
-      message: "Error al borrar el post: " + error.message,
-    };
+  const { rowCount } = await pool.query(consulta, values);
+  if (rowCount === 0) {
+    throw { code: 404, message: "No existe el post con id" };
   }
 };
 
--- backend/index.js
+++ backend/index.js
@@ -6,58 +6,56 @@ const {
 } = require("./funciones");
 const cors = require("cors");
 const express = require("express"); //importamos express
-const app = express(); //instanciamos express
+const app = express();
+
+require("dotenv").config();
+const morgan = require("morgan");
+//instanciamos express
 
 const port = 3000; //definimos puerto
 
 app.listen(port, () => console.log("Servidor escuchado en puerto 3000!")); //levantamos el servidor
 
 app.use(express.json()); //middleware
-app.use(cors());
+app.use(morgan("dev")); //morgan
+app.use(cors()); //CORS
+
+//Ruta leer posts
 
 app.get("/posts", async (req, res) => {
   const obtenerPost = await leerPost();
   res.json(obtenerPost);
 });
 
-app.post("/posts", async (req, res) => {
-  try {
-    const { titulo, url, descripcion } = req.body;
+//Ruta crear posts
 
-    if (!titulo || !url || !descripcion) {
-      return res
-        .status(400)
-        .send("Todos los campos (titulo, url, descripcion) son obligatorios.");
-    }
-    await escribirPost(titulo, url, descripcion);
-    res.send("El post fue agregado");
+app.post("/posts", async (req, res) => {
+  const { titulo, img, descripcion } = req.body;
+  try {
+    await escribirPost(titulo, img, descripcion);
+    res.status(201).json({ message: "El post fue agregado" });
   } catch (error) {
-    const { code } = error;
-    if (code == "23502") {
-      res
-        .status(400)
-        .send("Se ha violado la restricción NOT NUL en uno de los campos");
-    } else {
-      res.status(500).send(error);
-    }
+    res.status(500).json({ error: "Error de comunicación" });
   }
 });
 
+//Ruta dar like
 app.put("/posts/like/:id", async (req, res) => {
   const id = req.params.id;
   try {
     await agregarLike(id);
-    res.send("tenemos tu like");
+    res.send("Tenemos un like !!");
   } catch ({ code, message }) {
     res.status(code).send(message);
   }
 });
 
+//Ruta borrar posts
 app.delete("/posts/:id", async (req, res) => {
   const id = req.params.id;
   try {
     await borrarPost(id);
-    res.send("El post fue eliminado");
+    res.send("El posteo ha sido eliminado, pasó a mejor vida");
   } catch ({ code, message }) {
     res.status(code).send(message);
   }
